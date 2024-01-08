047cb898d2286d01ee5f236bdd3a76cc72a49e07
IKHLEF Sabrina <sabrina.ikhlef@hotmail.com>
2024-01-08 23:13:45 +0100

    ENH: start garden list visualization with group use

---
+++ FirstAppTest/Views/LandmarkRow.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkRow.swift
+//  FirstAppTest
+//
+//  Created by IKHLEF Sabrina on 08/01/2024.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View{
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
