22b1fe89bd8189d4885f9c6146470a2b0849b4e0
IKHLEF Sabrina <sabrina.ikhlef@hotmail.com>
2024-01-09 22:55:22 +0100

    ENH : add favorite bolean option and a way to filtered them

--- FirstAppTest/Views/LandmarkList.swift
+++ FirstAppTest/Views/LandmarkList.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = true // State alway private !
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter {landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
     var body: some View {
         NavigationSplitView{
-            List(landmarks) {landmark in
+            List(filteredLandmarks) {landmark in
                 NavigationLink{
                     LandmarkDetail(landmark: landmark)
                 } label: {
